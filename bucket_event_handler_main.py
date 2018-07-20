6618c3b8c9ad30f8004c999cc4919c697bd46dc7
swarnadee bapat <swarnadee.bapat@reancloud.com>
2018-07-20 21:38:19 +0530

    Persist data to kinesis

--- bucket_event_handler_main.py
+++ bucket_event_handler_main.py
@@ -1,19 +1,6 @@
-from bucket_handler_lambda.bucket_event_lambda_handler import *
+from kinesis_data_persist_handler.kinesis_event_lambda_handler import *
 
 
 def lambda_handler(event, context):
     handle_bucket_event = HandleBucketEvent()
     handle_bucket_event.handleBucketEvent(event, context)
-    curation_lambda_function_name = os.environ["CURATION_LAMBDA_FUNCTION_NAME"]
-    submissions_bucket_name = os.environ["SUBMISSIONS_BUCKET_NAME"]
-    sns_message = json.loads(event["Records"][0]["Sns"]["Message"])
-    bucket = sns_message["Records"][0]["s3"]["bucket"]["name"]
-
-    if bucket == submissions_bucket_name:
-        client = boto3.client('lambda')
-        LoggerUtility.logInfo("Invoking Curation lambda function")
-        response = client.invoke(FunctionName=curation_lambda_function_name,
-                                 InvocationType='Event',
-                                 Payload=json.dumps(event))
-        LoggerUtility.logInfo("Response from invoked lambda function - {}".format(response))
-
--- dashboard_registry_handler_main.py
+++
@@ -1,6 +0,0 @@
-from dashboard_registry_lambda.register_kibana_dashboard import *
-
-
-def lambda_handler(event, context):
-    register_kibana_dashboard = RegisterKibanaDashboard()
-    register_kibana_dashboard.registerKibanaDashboard()
