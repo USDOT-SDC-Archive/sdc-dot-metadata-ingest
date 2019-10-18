1aa49538f4cce58ce93384ade742acedff06f34b
HannahKent <hkent@acrotron.com>
2019-10-18 15:01:20 -0600

    SDC-1005 Code now pep8 compliant

--- bucket_event_handler_main.py
+++ bucket_event_handler_main.py
@@ -1,7 +1,7 @@
 from bucket_handler_lambda.bucket_event_lambda_handler import *
 
 
-def lambda_handler(event, context):
+def lambda_handler(event, *args, **kwargs):
     handle_bucket_event = HandleBucketEvent()
-    handle_bucket_event.handleBucketEvent(event, context)
+    handle_bucket_event.handle_bucket_event(event)
 
